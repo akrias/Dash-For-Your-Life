45a086298ec8a10712e4dc364140bc8f232d675c
Vincent Woo <Vincent@64-71-17-227.static.wiline.com>
2013-11-08 23:26:39 -0800

    Initial Commit

---
+++ Dash For Your Life/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dash For Your Life
+//
+//  Created by Vincent Woo on 11/8/13.
+//  Copyright (c) 2013 Vivid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dash For Your LifeTests/Dash_For_Your_LifeTests.m
@@ -0,0 +1,34 @@
+//
+//  Dash_For_Your_LifeTests.m
+//  Dash For Your LifeTests
+//
+//  Created by Vincent Woo on 11/8/13.
+//  Copyright (c) 2013 Vivid. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Dash_For_Your_LifeTests : XCTestCase
+
+@end
+
+@implementation Dash_For_Your_LifeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
