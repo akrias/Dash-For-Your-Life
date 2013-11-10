52c1553bf18cc72468c920b65d217d40521ee58b
David Lee <monodlee@gmail.com>
2013-11-09 19:43:21 -0800

    added view controller for selecting a destination in free mode parameters

---
+++ Dash For Your Life/DestinationSelectorViewController.h
@@ -0,0 +1,13 @@
+//
+//  DestinationSelectorViewController.h
+//  Dash For Your Life
+//
+//  Created by David Lee on 11/9/13.
+//  Copyright (c) 2013 Vivid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DestinationSelectorViewController : UIViewController
+
+@end
---
+++ Dash For Your Life/DestinationSelectorViewController.m
@@ -0,0 +1,38 @@
+//
+//  DestinationSelectorViewController.m
+//  Dash For Your Life
+//
+//  Created by David Lee on 11/9/13.
+//  Copyright (c) 2013 Vivid. All rights reserved.
+//
+
+#import "DestinationSelectorViewController.h"
+
+@interface DestinationSelectorViewController ()
+
+@end
+
+@implementation DestinationSelectorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
