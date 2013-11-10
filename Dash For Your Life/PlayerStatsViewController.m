5cf2c0485d1e3aeb3c53a9d35698bdab8e2a7efb
Vincent Woo <Vincent@64-71-17-230.static.wiline.com>
2013-11-10 00:43:56 -0800

    added title to given views

--- Dash For Your Life/PlayerStatsViewController.m
+++ Dash For Your Life/PlayerStatsViewController.m
@@ -28,6 +28,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor purpleColor];
+    self.title = @"Player Stats";
 }
 
 - (void)didReceiveMemoryWarning
